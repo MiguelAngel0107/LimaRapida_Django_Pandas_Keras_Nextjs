64e73649efbe80eb575fe3375a28b37456d72b7e
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-06-10 18:42:43 -0500

    update

--- backend/core/core/routers.py
+++ backend/core/core/routers.py
@@ -1,23 +1,25 @@
+App1 = "   "
+
 class MongodbRouter:
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'testMongo':
+        if model._meta.app_label == App1:
             return 'mongodb'
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'testMongo':
+        if model._meta.app_label == App1:
             return 'mongodb'
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
         if (
-            obj1._meta.app_label == 'testMongo' or
-            obj2._meta.app_label == 'testMongo'
+            obj1._meta.app_label == App1 or
+            obj2._meta.app_label == App1
         ):
             return True
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if app_label == 'testMongo':
+        if app_label == App1:
             return db == 'mongodb'
         return None
