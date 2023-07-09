ee2da46a3462563db0d42d8ce7f385990f8904bc
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-07-08 20:04:12 -0500

    Update globals.ts

--- frontend/view/src/globals.ts
+++ frontend/view/src/globals.ts
@@ -1,4 +1,4 @@
 export const APP_URL_HTTP_BACK: string =
   "https://investigacion-redes-neuronales.onrender.com"; // https://investigacion-redes-neuronales.onrender.com  // http://192.168.1.5:8000
 export const APP_URL_WS_BACK: string =
-  "wss://investigacion-redes-neuronales.onrender.com  "; // wss://investigacion-redes-neuronales.onrender.com    // ws://192.168.1.5:8000
+  "wss://investigacion-redes-neuronales.onrender.com"; // wss://investigacion-redes-neuronales.onrender.com    // ws://192.168.1.5:8000
