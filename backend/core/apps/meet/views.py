23834bf48c21c976176aa1470cc75a362fd14a28
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-07-20 11:33:06 -0500

    V2_CollectData

--- backend/core/apps/meet/views.py
+++ backend/core/apps/meet/views.py
@@ -4,30 +4,38 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 from django.conf import settings
 from .models import ImagenCollection
-
+from core.storage_backeds import MediaStorage
+from PIL import Image
 import os
 import datetime
+import io
+
 
 class GuardarImagenAPIView(APIView):
     permission_classes = [permissions.AllowAny]
+
     def post(self, request, format=None):
         imagen_file = request.FILES.get('imagen')
 
         if imagen_file:
-            # Guardar la imagen en la carpeta de medios
-            nombre_archivo = f'{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.png'
-            
-            ruta_guardado = os.path.join(settings.MEDIA_ROOT, nombre_archivo)
-            
-            with open(ruta_guardado, 'wb') as f:
-                for chunk in imagen_file.chunks():
-                    f.write(chunk)
+            # Validar que sea una imagen
+            if not imagen_file.content_type.startswith('image/'):
+                return Response({'error': 'Invalid file type. Only images are allowed.'}, status=status.HTTP_400_BAD_REQUEST)
 
-            nombre_archivo = f'https://investigacion-redes-neuronales.onrender.com/media/{nombre_archivo}'
+            try:
+                # Guarda la imagen en S3 usando MediaStorage
+                image_path = f'{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.png'
+                media_storage = MediaStorage()
+                media_storage.save(image_path, imagen_file)
 
-            ImagenCollection.objects.create(nombre=nombre_archivo)
-            print("Todo Salio Bien Se Guardo")
+                nombre_archivo = f'https://redes-neuronales.s3.amazonaws.com/media/{image_path}'
+                
+                # Crear la entrada en la base de datos
+                ImagenCollection.objects.create(nombre=nombre_archivo)
+                print("Todo Salio Bien Se Guardo")
 
-            return Response({'mensaje': 'Imagen guardada correctamente.'}, status=status.HTTP_201_CREATED)
+                return Response({'mensaje': 'Imagen guardada correctamente.'}, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         print("Nada Salio Bien No se Guardo")
         return Response({'error': 'No se recibió una imagen válida.'}, status=status.HTTP_400_BAD_REQUEST)
