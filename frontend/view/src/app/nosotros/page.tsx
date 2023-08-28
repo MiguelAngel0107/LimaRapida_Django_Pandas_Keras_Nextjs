56ddfa139425163ea029cfb063de5bee29b61b2d
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-08-27 19:10:33 -0500

    MejorasEnTerminosYcondiciones

--- frontend/view/src/app/nosotros/page.tsx
+++ frontend/view/src/app/nosotros/page.tsx
@@ -1,7 +1,71 @@
 import React from "react";
 
 function Page() {
-  return <div>Page</div>;
+  return (
+    <div className="text-gray-100 p-4">
+      <h1 className="text-3xl font-semibold mb-4">
+        Términos y Condiciones de Uso
+      </h1>
+
+      <h2 className="text-2xl font-semibold mb-2">
+        Plataforma de Videoconferencias y Mensajería
+      </h2>
+
+      <p className="mb-4">
+        Bienvenido a nuestra plataforma de videoconferencias y mensajería (en
+        adelante, "el Servicio"). Al utilizar nuestro Servicio, usted acepta los
+        siguientes términos y condiciones:
+      </p>
+
+      <h3 className="text-xl font-semibold mb-2">1. Uso del Servicio</h3>
+      <p className="mb-2">
+        1.1. Usted utilizará el Servicio únicamente con fines académicos y de
+        entrenamiento de redes neuronales.
+      </p>
+      <p className="mb-4">
+        1.2. Usted cumplirá con todas las leyes y regulaciones aplicables al
+        utilizar el Servicio.
+      </p>
+
+      <h3 className="text-xl font-semibold mb-2">
+        2. Privacidad y Datos Personales
+      </h3>
+      <p className="mb-2">
+        2.1. Los datos recopilados, como el número de usuarios entrantes por día
+        y las secciones más vistas, serán almacenados de manera anónima y
+        agregada. No se recopilarán ni almacenarán datos personales
+        identificables para estos fines.
+      </p>
+      <p className="mb-4">
+        2.2. Garantizamos la seguridad y confidencialidad de estos datos
+        anónimos de acuerdo con nuestra Política de Privacidad.
+      </p>
+
+      <h3 className="text-xl font-semibold mb-2">
+        3. Cambios en los Términos y Condiciones
+      </h3>
+      <p className="mb-4">
+        3.1. Nos reservamos el derecho de modificar estos términos y condiciones
+        en cualquier momento. Los cambios entrarán en vigencia después de su
+        publicación en nuestro sitio web.
+      </p>
+
+      <h3 className="text-xl font-semibold mb-2">4. Contacto</h3>
+      <p className="mb-4">
+        4.1. Para cualquier pregunta o inquietud relacionada con estos términos
+        y condiciones, póngase en contacto con nosotros a través de nuestro
+        sitio web.
+      </p>
+
+      <p className="mb-2">
+        Al utilizar nuestro Servicio, usted acepta cumplir con estos términos y
+        condiciones. Si no está de acuerdo con estos términos, no utilice el
+        Servicio.
+      </p>
+
+      <p>Fecha de última actualización: domingo, 27 de agosto de 2023</p>
+    </div>
+  );
 }
 
 export default Page;
